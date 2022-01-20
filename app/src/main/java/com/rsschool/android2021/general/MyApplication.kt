cac71cb9821c3218364ba38f93b2b6ed75f90f45
Sergey <serg.karpik@gmail.com>
2022-01-20 20:11:52 +0300

    starting adding Cicerone lib for navigation

---
+++ app/src/main/java/com/rsschool/android2021/general/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.rsschool.android2021.general
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
